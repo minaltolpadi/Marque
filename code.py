fcf840f7d6fd264ddb125001e7f7ecd91dd0ce73
Minal <minaltolpadi@gmail.com>
2016-04-13 10:47:18 -0400

    Identify positive sentiments

---
+++ code.py
@@ -0,0 +1,31 @@
+import tweepy
+import json
+import textblob
+
+
+brand = "trustfuel"
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_data(self, data):
+    	#json to formatted data conversion
+        formatted_data = json.loads(data)
+        #check if the sentiment is positive
+        if textblob.TextBlob(formatted_data["text"]).sentiment.polarity>0:
+        	#print in format "username | message" 
+        	print "@" + formatted_data["user"]["screen_name"] + " | " + formatted_data["text"] + "\n"
+
+
+if __name__ == '__main__':
+
+	#authentication
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	#create an instance
+	myStreamListener = MyStreamListener()
+	myStream = tweepy.Stream(auth,myStreamListener)
+	#filter according to the brand name
+	myStream.filter(track=[brand])
+
+
+
---
+++ conf.py
@@ -0,0 +1,5 @@
+consumer_key = "PTswHyxxpIC62QciyqZSxmqSr"
+consumer_secret = "BT6v3XZvPRo2raexq908L49J0ST3GMRu3Q90Pb3irknXTFTUzU"
+access_token = "720240476201168896-ZI3kFw1SPwVrvF9RDPdaeqs9iIwvSQ2"
+access_token_secret = "WWPnMg6kA4HCekpfVtxtjhWiAIQ6uv5ujQ8Q9Y2U2Rj3d"
+
