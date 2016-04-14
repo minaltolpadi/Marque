9c9257c42b78644630addad3c224646182acb160
minaltolpadi <minaltolpadi@gmail.com>
2016-04-14 13:25:50 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
-consumer_key = "PTswHyxxpIC62QciyqZSxmqSr"
-consumer_secret = "BT6v3XZvPRo2raexq908L49J0ST3GMRu3Q90Pb3irknXTFTUzU"
-access_token = "720240476201168896-ZI3kFw1SPwVrvF9RDPdaeqs9iIwvSQ2"
-access_token_secret = "WWPnMg6kA4HCekpfVtxtjhWiAIQ6uv5ujQ8Q9Y2U2Rj3d"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 
